52d0a7ea7c022ef569d03dbb3603ff53e1b99123
Immanuel Diaz <immanuel.diaz@includedhealth.com>
2025-03-18 19:29:13 -0600

    update 1.0.18

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.ih.osm"
         minSdk = 24
         targetSdk = 35
-        versionCode = 22
-        versionName = "1.0.17"
+        versionCode = 23
+        versionName = "1.0.18"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
